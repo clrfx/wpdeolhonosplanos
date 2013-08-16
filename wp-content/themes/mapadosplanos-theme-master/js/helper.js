4828b57030287730320ef60b3771d41af220b059
Vinicius Massuchetto <vmassuchetto@gmail.com>
2013-08-15 22:56:22 -0300

    Zoom inicial do mapa da home

--- wp-content/themes/mapadosplanos-theme-master/js/helper.js
+++ wp-content/themes/mapadosplanos-theme-master/js/helper.js
@@ -23,8 +23,8 @@ $(document).ready(function () {
             map.interaction.auto(); 
         }));
 
-        map.zoom(4).center({ lat: -13.32, lon: -54.15 });
-        map.setZoomRange(4, 10);
+        map.zoom(5).center({ lat: -09.32, lon: -54.15 });
+        map.setZoomRange(5, 10);
         map.setPanLimits([{ lat: -34.1618, lon: -75.0146 }, { lat:6.0532 , lon: -31.8603 }]);
         map.addLayer(data[1].layer, function() {
             map.interaction.auto();
