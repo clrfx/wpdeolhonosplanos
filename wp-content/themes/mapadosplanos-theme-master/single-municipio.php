c1aec667a2d1bc2dfb68c187ac1c56f3654e883b
Diego Rojas <diego@brasa.art.br>
2013-11-21 17:54:36 -0200

    somando template de commet no tema e alterando chamada da função no single-municipio

--- wp-content/themes/mapadosplanos-theme-master/single-municipio.php
+++ wp-content/themes/mapadosplanos-theme-master/single-municipio.php
@@ -13,10 +13,10 @@ get_header('sem-main'); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'content', 'municipio' ); ?>
-				
-				<?php comments_template( '', true ); ?>
 			
 			<?php endwhile; // end of the loop. ?>
+			
+				<?php comments_template(); ?>
 
 		</div><!-- #content.site-content -->
 				
