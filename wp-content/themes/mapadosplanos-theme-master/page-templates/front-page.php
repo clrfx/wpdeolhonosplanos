d59eccc360b3fff6d053b2061cbfee6b63abbfd5
Eduardo Zulian <eduardozulian@gmail.com>
2014-10-20 14:44:28 -0200

    Remove as áreas antigas de destaque e notícias

--- wp-content/themes/mapadosplanos-theme-master/page-templates/front-page.php
+++ wp-content/themes/mapadosplanos-theme-master/page-templates/front-page.php
@@ -14,19 +14,6 @@
 
 get_header(); ?>
 
-	<div id="primary" class="site-home">
-
-		<?php //if(function_exists('wp_content_slider')) { wp_content_slider(); } ?>
-		Aqui ficava o slider
-
-	</div><!-- #primary -->
-
-	<?php get_sidebar('front'); ?>
-
-	<div class="clear"></div>
-
-	<hr/>
-
 	<div class="featured-content">
 		<div class="featured-slider content-slider col-3">
 			<?php if(function_exists('wp_content_slider')) { wp_content_slider(); } ?>
@@ -159,47 +146,6 @@ get_header(); ?>
 </div><!-- .secondary-content -->
 
 <div class="clear"></div>
-
-<div id="title-querys" class="noticias">
-	<div class="triangulo triangulo-noticias">
-	</div>
-	<h1 class="noticias"><a href="<?php echo esc_url( home_url( '/category/noticias/' ) ); ?>">Not&iacute;cias</a></h1>
-</div>
-
-<div id="query-posts" class="noticias">
-
-	<?php
-		if ( query_posts( array ( 'cat' => '-13,-29,-28,-30','post_status' => 'publish', 'posts_per_page' => 3 ) ) ) : while ( have_posts() ) : the_post(); 
-	?>
-
-		<div id="cada-post" <?php post_class(); ?>>
-			<a class="link-img-destacada" href="<?php echo get_permalink(); ?>">
-				<?php 
-				if ( has_post_thumbnail() ) {
-					the_post_thumbnail('category-sticky', array('class'	=> "category-image"));
-				}
-				else {
-					echo '<img class="category-image" src="' . get_bloginfo( 'stylesheet_directory' ) . '/img/thumbnail-default-large.png" />';
-				} 
-				?>
-			</a>
-
-			<h1 class="entry-title-query"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
-			<div class="entry-content-post-home">
-				<?php echo excerpt( 20 ); //Imprime 21 palavras ?> ...
-				<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentytwelve' ), 'after' => '</div>' ) ); ?>
-				<div class="entry-utility">
-					<?php edit_post_link( __('Edit', 'twentyten' ), '<span class="edit-link">', '</span>' ); ?>
-				</div><!-- .entry-utility -->
-			</div><!-- .entry-content -->
-			
-			<a href="<?php the_permalink(); ?>" rel="bookmark" class="leia-mais noticias-bg"><span class="mais">+</span></a>
-		</div><!-- #cada-post -->
-
-	<?php endwhile; endif; // end of the loop. ?>
-	<?php wp_reset_query(); // reset query ?>
-
-</div>
 
 <div class="col-3">
 	<h3 class="area-title"><a href="<?php echo get_post_format_link( 'video' ); ?>">Vídeo</a></h3>
