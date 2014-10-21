2770e26df7b0834dcf93a8ddca9e8c971e4f21c2
Eduardo Zulian <eduardozulian@gmail.com>
2014-10-21 09:56:05 -0200

    Otimiza as queries da capa
    Também adiciona o feed de atividades do FB e uma nova classe .pane para
    separação de conteúdo

--- wp-content/themes/mapadosplanos-theme-master/page-templates/front-page.php
+++ wp-content/themes/mapadosplanos-theme-master/page-templates/front-page.php
@@ -14,95 +14,69 @@
 
 get_header(); ?>
 
-	<div class="featured-content">
-		<div class="featured-slider content-slider col-3">
-			<?php if(function_exists('wp_content_slider')) { wp_content_slider(); } ?>
-		</div>
+<div class="featured-content pane clearfix">
+	<div class="featured-slider content-slider col-3">
+		<?php if(function_exists('wp_content_slider')) { wp_content_slider(); } ?>
+	</div>
 
-		<div class="news-main col-3">
-			<?php
-			$destaques = new WP_Query( array(
-				'ignore_sticky_posts' => true,
-				'cat' => '-13,-29,-28,-30',
-				'post_status' => 'publish',
-				'posts_per_page' => 1,
-			    'tax_query' => array(
-			        array(                
-			            'taxonomy' => 'post_format',
-			            'field' => 'slug',
-			            'terms' => array( 
-			                'post-format-aside',
-			                'post-format-audio',
-			                'post-format-chat',
-			                'post-format-gallery',
-			                'post-format-image',
-			                'post-format-link',
-			                'post-format-quote',
-			                'post-format-status',
-			                'post-format-video'
-			            ),
-			            'operator' => 'NOT IN'
-			        )
-			    )
-			) );
+	<?php
+	$destaques = new WP_Query( array(
+		'ignore_sticky_posts' => true,
+		'cat' => '-13,-29,-28,-30',
+		'post_status' => 'publish',
+		'posts_per_page' => 4,
+	    'tax_query' => array(
+	        array(                
+	            'taxonomy' => 'post_format',
+	            'field' => 'slug',
+	            'terms' => array( 
+	                'post-format-aside',
+	                'post-format-audio',
+	                'post-format-chat',
+	                'post-format-gallery',
+	                'post-format-image',
+	                'post-format-link',
+	                'post-format-quote',
+	                'post-format-status',
+	                'post-format-video'
+	            ),
+	            'operator' => 'NOT IN'
+	        )
+	    )
+	) );
 
-			if ( $destaques->have_posts() ) : while ( $destaques->have_posts() ) : $destaques->the_post(); ?>
+	if ( $destaques->have_posts() ) : 
 
-			<div <?php post_class(); ?>>
-				<a class="link-img-destacada" href="<?php echo get_permalink(); ?>">
-					<?php 
-					if ( has_post_thumbnail() ) {
-						the_post_thumbnail('category-sticky', array('class'	=> "category-image"));
-					}
-					else {
-						echo '<img class="category-image" src="' . get_bloginfo( 'stylesheet_directory' ) . '/img/thumbnail-default-large.png" />';
-					} 
-					?>
-				</a>
+		$i = 1;
+		while ( $destaques->have_posts() ) : $destaques->the_post(); ?>
 
-				<h1 class="entry-title-query"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
-				<div class="entry-content-post-home">
-					<?php echo excerpt( 30 ); //Imprime 21 palavras ?> &hellip;
-					<div class="entry-utility">
-						<?php edit_post_link( __('Edit', 'twentyten' ), '<span class="edit-link">', '</span>' ); ?>
-					</div><!-- .entry-utility -->
-				</div><!-- .entry-content -->
-			</div><!-- #cada-post -->
+			<?php if ( $i == 1 ) : ?>
 
-			<?php endwhile; endif; // end of the loop. ?>
-		<?php wp_reset_query(); // reset query ?>
-		</div>
+			<div class="news-main col-3">
+				<div <?php post_class(); ?>>
+					<a class="link-img-destacada" href="<?php echo get_permalink(); ?>">
+						<?php 
+						if ( has_post_thumbnail() ) {
+							the_post_thumbnail('category-sticky', array('class'	=> "category-image"));
+						}
+						else {
+							echo '<img class="category-image" src="' . get_bloginfo( 'stylesheet_directory' ) . '/img/thumbnail-default-large.png" />';
+						} 
+						?>
+					</a>
 
-		<div class="news-list col-3">
-			<?php
-			$mais_noticias = new WP_Query( array(
-				'ignore_sticky_posts' => true,
-				'cat' => '-13,-29,-28,-30',
-				'post_status' => 'publish',
-				'posts_per_page' => 3,
-				'offset' => 1,
-			    'tax_query' => array(
-			        array(                
-			            'taxonomy' => 'post_format',
-			            'field' => 'slug',
-			            'terms' => array( 
-			                'post-format-aside',
-			                'post-format-audio',
-			                'post-format-chat',
-			                'post-format-gallery',
-			                'post-format-image',
-			                'post-format-link',
-			                'post-format-quote',
-			                'post-format-status',
-			                'post-format-video'
-			            ),
-			            'operator' => 'NOT IN'
-			        )
-			    )
-			) );
+					<h1 class="entry-title-query"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+					<div class="entry-content-post-home">
+						<?php the_excerpt(); ?>
+						<div class="entry-utility">
+							<?php edit_post_link( __('Edit', 'twentyten' ), '<span class="edit-link">', '</span>' ); ?>
+						</div><!-- .entry-utility -->
+					</div><!-- .entry-content -->
+				</div><!-- .post -->
+			</div><!-- .news-main .col-3 -->
 
-			if ( $mais_noticias->have_posts() ) : while ( $mais_noticias->have_posts() ) : $mais_noticias->the_post(); ?>
-
+			<div class="news-list col-3">
+			<?php else : ?>
 
 				<div <?php post_class( 'media' ); ?>>
 					<a class="alignleft" href="<?php echo get_permalink(); ?>">
@@ -121,16 +95,23 @@ get_header(); ?>
 							<?php edit_post_link( __('Edit', 'twentyten' ), '<span class="edit-link">', '</span>' ); ?>
 						</div><!-- .entry-utility -->
 					</div><!-- .media-body -->
-				</div><!-- #cada-post -->
+				</div><!-- .media -->
+			<?php endif; // if ( $i == 1 ) ?>
 
-			<?php endwhile; endif; // end of the loop. ?>
-			<?php wp_reset_query(); // reset query ?>
-		</div>
-	</div><!-- .featured-content -->
+		<?php
+		$i++;
+		endwhile; ?>
+
+			</div><!-- .news-list .col-3 -->
+
+		<?php endif; // end of the loop. ?>
+
+		<?php wp_reset_query(); // reset query ?>
+</div><!-- .featured-content -->
 
 <div class="clear"></div>
 
-<div class="secondary-content">
+<div class="secondary-content pane clearfix">
 	<div class="container-mapa col col-2-3">
 		<h3 class="area-title">De Olho no Mapa</h3>
 		<div id="barra-mapa">
@@ -193,7 +174,7 @@ get_header(); ?>
 
 <div class="clear"></div>
 
-<div class="tertiary-content clearfix">
+<div class="tertiary-content pane clearfix">
 	<div class="col-3">
 		<h3 class="area-title"><a href="<?php echo get_post_format_link( 'video' ); ?>">Vídeos</a></h3>
 		<?php
@@ -228,11 +209,12 @@ get_header(); ?>
 			<h3 class="noticias area-title"><a href="<?php echo esc_url( home_url( '/category/noticias/' ) ); ?>">Notícias</a></h3>
 			<?php
 			$mais_noticias = new WP_Query( array(
-				'ignore_sticky_posts' => true,
-				'cat' => '-13,-29,-28,-30',
-				'post_status' => 'publish',
-				'posts_per_page' => 3,
-			    'tax_query' => array(
+				'ignore_sticky_posts' 	=> true,
+				'cat' 					=> '-13,-29,-28,-30',
+				'post_status' 			=> 'publish',
+				'posts_per_page' 		=> 3,
+				'offset'				=> 4,
+			    'tax_query' 			=> array(
 			        array(                
 			            'taxonomy' => 'post_format',
 			            'field' => 'slug',
@@ -280,7 +262,7 @@ get_header(); ?>
 
 	<div class="col-3">
 		<h3 class="area-title"><a href="https://www.facebook.com/DeOlhoNosPlanos">No Facebook</a></h3>
-		Feed de atividades do FB
+		<iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2FDeOlhoNosPlanos&amp;width&amp;height=395&amp;colorscheme=light&amp;show_faces=false&amp;header=false&amp;stream=true&amp;show_border=true&amp;appId=341357382712484" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:395px;" allowTransparency="true"></iframe>
 	</div><!-- .col-3 -->
 </div><!-- .tertiary-content -->
 
