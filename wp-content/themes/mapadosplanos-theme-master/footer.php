0f5614262e2869d403fb4b1dd48c86f4cf3d2de8
Eduardo Zulian <eduardozulian@gmail.com>
2014-10-23 16:59:14 -0200

    Adiciona colunas ao rodapé

--- wp-content/themes/mapadosplanos-theme-master/footer.php
+++ wp-content/themes/mapadosplanos-theme-master/footer.php
@@ -36,9 +36,9 @@
 			<div class="site-info-general site-info-col">
 				<span class="site-name"><?php bloginfo( 'name' ); ?></span>
 				<br/>
-				Telefone: (11) 3151-2333 - Ramais 130 e 170
+				Telefone: (11) 3151-2333 &ndash; Ramais 130 e 170
 				<br/><a href="<?php echo esc_url( home_url( '/ajuda/' ) ); ?>">Entre em contato</a>
-				<br/>
+				<br/><br/>
 				<ul class="site-credits">
 					<a href="<?php echo esc_url( home_url( '/termo-de-uso/' ) ); ?>">Termo de uso</a> &bull; 
 					<a href="<?php echo esc_url( home_url( '/creditos/' ) ); ?>">Créditos</a> &bull; 
@@ -46,7 +46,7 @@
 				</ul><!-- .site-credits -->
 			</div>
 			<div class="site-info-cc site-info-col">
-				Este trabalho está licenciado com uma Licença <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons &ndash; Atribuição 4.0 Internacional</a>.<br/>
+				Este trabalho está licenciado com uma Licença <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons &ndash; Atribuição 4.0 Internacional</a>.
 				Esta licença permite que outros distribuam, remixem, adaptem e criem a partir do seu trabalho, mesmo para fins comerciais, desde que lhe atribuam o devido crédito pela criação original.
 				<br/><br/><a rel="license" href="http://creativecommons.org/licenses/by/3.0/br/" title="Este obra está licenciado com uma Licença Creative Commons Atribuição 3.0 Brasil"><img alt="Licença Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by/3.0/br/80x15.png" /></a>
 			</div>
