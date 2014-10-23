d903b82a96359e3934f1316f63986c2ad5c58284
Eduardo Zulian <eduardozulian@gmail.com>
2014-10-23 11:07:05 -0200

    Adiciona texto do CC no rodapé

--- wp-content/themes/mapadosplanos-theme-master/footer.php
+++ wp-content/themes/mapadosplanos-theme-master/footer.php
@@ -13,35 +13,43 @@
 	</div><!-- #main .wrapper -->
 	<footer id="colophon" role="contentinfo">
 		<div class="row-fluid">
-		<div id="footer-a">
-			<?php if ( is_active_sidebar( 'footer-a' ) ) : ?>
-				<?php dynamic_sidebar( 'footer-a' ); ?>
-			<?php endif; ?>
+			<div id="footer-a">
+				<?php if ( is_active_sidebar( 'footer-a' ) ) : ?>
+					<?php dynamic_sidebar( 'footer-a' ); ?>
+				<?php endif; ?>
+			</div>
+			
+			<div id="footer-b">
+				<?php if ( is_active_sidebar( 'footer-b' ) ) : ?>
+					<?php dynamic_sidebar( 'footer-b' ); ?>
+				<?php endif; ?>
+			</div>
+			
+			<div id="footer-c">
+				<?php if ( is_active_sidebar( 'footer-c' ) ) : ?>
+					<?php dynamic_sidebar( 'footer-c' ); ?>
+				<?php endif; ?>
+			</div>
 		</div>
-		
-		<div id="footer-b">
-			<?php if ( is_active_sidebar( 'footer-b' ) ) : ?>
-				<?php dynamic_sidebar( 'footer-b' ); ?>
-			<?php endif; ?>
-		</div>
-		
-		<div id="footer-c">
-			<?php if ( is_active_sidebar( 'footer-c' ) ) : ?>
-				<?php dynamic_sidebar( 'footer-c' ); ?>
-			<?php endif; ?>
-		</div>
-		
-		</div>
-			<div class="site-info">
-				<strong><?php bloginfo( 'name' ); ?></strong>
+
+		<div class="site-info clearfix">
+			<div class="site-info-general site-info-col">
+				<span class="site-name"><?php bloginfo( 'name' ); ?></span>
 				<br/>
 				Telefone: (11) 3151-2333 - Ramais 130 e 170 &bull; <a href="<?php echo esc_url( home_url( '/ajuda/' ) ); ?>">Entre em contato</a>
-				<br/> 
-				<a href="<?php echo esc_url( home_url( '/termo-de-uso/' ) ); ?>">Termo de uso</a> &bull; 
-				<a href="<?php echo esc_url( home_url( '/creditos/' ) ); ?>">Créditos</a> &bull; 
-				<a href="http://creativecommons.org/licenses/by-nc-sa/3.0/deed.pt" target="_blank">Creative Commons</a> &bull; 
-				<a href="<?php echo esc_url( __( 'http://br.wordpress.org/', 'twentytwelve' ) ); ?>" title="<?php esc_attr_e( 'Desenvolvido com WordPress', 'twentytwelve' ); ?>">WordPress</a>
+				<br/>
+				<ul class="site-credits">
+					<a href="<?php echo esc_url( home_url( '/termo-de-uso/' ) ); ?>">Termo de uso</a> &bull; 
+					<a href="<?php echo esc_url( home_url( '/creditos/' ) ); ?>">Créditos</a> &bull; 
+					<a href="<?php echo esc_url( __( 'http://br.wordpress.org/', 'twentytwelve' ) ); ?>" title="<?php esc_attr_e( 'Desenvolvido com WordPress', 'twentytwelve' ); ?>">WordPress</a>
+				</ul><!-- .site-credits -->
 			</div>
+			<div class="site-info-cc site-info-col">
+				Esta obra está licenciada com uma licença <a rel="license" href="http://creativecommons.org/licenses/by/3.0/br/">Creative Commons Atribuição 3.0 Brasil</a>.<br/>
+				Esta licença permite que outros distribuam, remixem, adaptem e criem a partir do seu trabalho, mesmo para fins comerciais, desde que lhe atribuam o devido crédito pela criação original. É a licença mais flexível de todas as licenças disponíveis. É recomendada para maximizar a disseminação e uso dos materiais licenciados.
+				<br/><br/><a rel="license" href="http://creativecommons.org/licenses/by/3.0/br/" title="Este obra está licenciado com uma Licença Creative Commons Atribuição 3.0 Brasil"><img alt="Licença Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by/3.0/br/80x15.png" /></a>
+			</div>
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
